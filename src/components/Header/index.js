82995235c1b4ae16f195cf2597894cca55bad787
braagaa <michael_braga@outlook.com>
2021-05-06 09:30:56 -0400

    changed env file

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -12,7 +12,6 @@ const Header = function () {
   const { logoutUser } = useUserState();
 
   const handleLogout = () => {
-    //change this to directory
     logoutUser();
     history.replace("/login");
   };
