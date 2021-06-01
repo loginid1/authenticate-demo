f78a80c80dafcc5b4867a2078d378ebec04c5430
braagaa <michael_braga@outlook.com>
2021-06-01 17:25:59 -0400

    TransactionComplete mobile complete

--- src/views/TransactionComplete/index.js
+++ src/views/TransactionComplete/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import style from "./style.module.css";
 
+import { currentDay } from "../../utils/date";
+
 import Header from "../../components/Header/";
+import { BoldRow } from "../../components/Row/";
+import Button from "../../components/Button/";
 import { ReactComponent as CheckMark } from "../../imgs/checkmark1.svg";
 
 const TransactionComplete = function () {
@@ -14,9 +18,29 @@ const TransactionComplete = function () {
         </div>
         <div>Transaction Approved</div>
       </div>
-      <div className={style.note}>
-        Thank you, your payment has been received. A confirmation email has been
-        sent to username.
+      <div className={style.info}>
+        <div className={style.note}>
+          Thank you, your payment has been received.{" "}
+          <span className={style.normal}>
+            A confirmation email has been sent to
+          </span>{" "}
+          <span className={style.email}>username.</span>
+        </div>
+        <div className={style.spacer}>
+          <div>Payment Details:</div>
+          <hr />
+        </div>
+        <div className={style.rows}>
+          <BoldRow title="Date:" value={currentDay()} />
+          <BoldRow title="Transfer:" value="USD" />
+          <BoldRow title="Amount:" value="$937.00" />
+          <BoldRow title="To:" value="YYZ Financial" />
+          <BoldRow title="Fee:" value="$0.01" />
+        </div>
+        <div className={style.buttons}>
+          <Button secondary text="Pay Another Bill" />
+          <Button text="View My Accounts" />
+        </div>
       </div>
     </div>
   );
