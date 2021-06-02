ecf34f1a47ac4aa6183c92be19d2852cfb8c058b
braagaa <michael_braga@outlook.com>
2021-06-02 01:50:41 -0400

    TransactionComplete desktop complete

--- src/views/TransactionComplete/index.js
+++ src/views/TransactionComplete/index.js
@@ -5,41 +5,48 @@ import { currentDay } from "../../utils/date";
 
 import Header from "../../components/Header/";
 import { BoldRow } from "../../components/Row/";
-import Button from "../../components/Button/";
+import Button, { SmallButton } from "../../components/Button/";
 import { ReactComponent as CheckMark } from "../../imgs/checkmark1.svg";
 
 const TransactionComplete = function () {
   return (
-    <div className={style.wrapper}>
+    <div>
       <Header />
-      <div className={style.complete}>
-        <div className={style.checkmark}>
-          <CheckMark />
+      <div className={style.wrapper}>
+        <div className={style.backButton}>
+          <SmallButton secondary text="Back to Accounts" />
         </div>
-        <div>Transaction Approved</div>
-      </div>
-      <div className={style.info}>
-        <div className={style.note}>
-          Thank you, your payment has been received.{" "}
-          <span className={style.normal}>
-            A confirmation email has been sent to
-          </span>{" "}
-          <span className={style.email}>username.</span>
-        </div>
-        <div className={style.spacer}>
-          <div>Payment Details:</div>
-          <hr />
-        </div>
-        <div className={style.rows}>
-          <BoldRow title="Date:" value={currentDay()} />
-          <BoldRow title="Transfer:" value="USD" />
-          <BoldRow title="Amount:" value="$937.00" />
-          <BoldRow title="To:" value="YYZ Financial" />
-          <BoldRow title="Fee:" value="$0.01" />
-        </div>
-        <div className={style.buttons}>
-          <Button secondary text="Pay Another Bill" />
-          <Button text="View My Accounts" />
+        <div className={style.inner}>
+          <div className={style.complete}>
+            <div className={style.checkmark}>
+              <CheckMark />
+            </div>
+            <div>Transaction Approved</div>
+          </div>
+          <div className={style.info}>
+            <div className={style.note}>
+              Thank you, your payment has been received.{" "}
+              <span className={style.normal}>
+                A confirmation email has been sent to
+              </span>{" "}
+              <span className={style.email}>username.</span>
+            </div>
+            <div className={style.spacer}>
+              <div>Payment Details:</div>
+              <hr />
+            </div>
+            <div className={style.rows}>
+              <BoldRow title="Date:" value={currentDay()} />
+              <BoldRow title="Transfer:" value="USD" />
+              <BoldRow title="Amount:" value="$937.00" />
+              <BoldRow title="To:" value="YYZ Financial" />
+              <BoldRow title="Fee:" value="$0.01" />
+            </div>
+            <div className={style.buttons}>
+              <Button secondary text="Pay Another Bill" />
+              <Button text="View My Accounts" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
